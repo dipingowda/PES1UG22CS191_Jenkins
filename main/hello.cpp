b80768791dd132871c46f2f55611e1f3c9b41f9a
Dipin <Dipin@DESKTOP-HIALVLH>
2025-03-13 23:35:29 +0530

    NGROK

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,7 +1,13 @@
+//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello from Jenkins!" << endll; // Intentional error
-    return 0;
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Dipin made and ran this on NGROK.  \n";
+  
+  return 0;
 }
+// Webhook Test
+// Webhook Test
